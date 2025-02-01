49d4592f2f9e87565e5d252cca674b8744a802ff
David Kalmuk <correo@correo.com>
2025-02-01 13:48:40 +0100

    Añadimos la conexion a nuestra base de datos y la dejamos lista para usarla

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 from flask import Flask,render_template,flash,url_for,redirect
 from flask_login import LoginManager, login_user,logout_user, login_required,current_user
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app=Flask(__name__)
+conexion=mysql.connector.connect(
+    host=os.getenv('HOST'),
+    user=os.getenv('USER'),
+    password=os.getenv('PASSWORD'),
+    db=os.getenv('DATABASE')
+)
 
+print(conexion)
 
 @app.route('/')#ruta que enviará directamente al login
 def index():
